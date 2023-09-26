2519270ac1e5c4e31440e26c5054bc1e5b2055c2
Sulaiman <salluansari777890@gmail.com>
2023-09-26 14:40:47 -0700

    practice

---
+++ Javascript backgroundColor/js/script.js
@@ -0,0 +1,7 @@
+const changeColor = (color) => {
+    document.body.style.backgroundColor = color
+}
+
+const display = (hidden) => {
+    document.getElementById("image").style.display = hidden
+} 
